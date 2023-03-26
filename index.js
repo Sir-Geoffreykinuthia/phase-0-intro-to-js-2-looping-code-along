41bebc895a1141a02b9b9fddc92e16ea9e9cd525
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-03-26 15:27:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    names.forEach(name => {
+      const message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    });
+    return messages;
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const event = "surprise gift";
+  const messages = writeCards(names, event);
+  
+  console.log(messages);
+
+
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+
+
+  
